99a492e1ade339b45a63904f445dcdcbce49693c
a-samir97 <a.samir9710@gmail.com>
2020-11-10 13:59:42 +0200

    Created handlers folder for business logic act as Controller in MVC

---
+++ backend/handlers/users.go
@@ -0,0 +1,21 @@
+package handlers
+
+import "net/http"
+
+type UserController struct{}
+
+func (user *UserController) createUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (user *UserController) updateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (user *UserController) getUserByID(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (user *UserController) deleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
