40afeb4f40519d91524842161d2a9bf03b3ff451
Ahmed Samir <a.samir9710@gmail.com>
2020-11-26 14:58:11 +0200

    added travis db configuration

--- backend/helpers/db.go
+++ backend/helpers/db.go
@@ -5,13 +5,21 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq" // sql behavior modified
 )
 
 func InitDb() (*sql.DB, error) {
-	var connectionString = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		config.Host, config.Port, config.User, config.Password, config.Dbname)
+	var connectionString string
+
+	if os.Getenv("BUILD_ON_TRAVIS") == "true" {
+		connectionString = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			"127.0.0.1", 5432, "travis", "", "travis_ci_test")
+	} else {
+		connectionString = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			config.Host, config.Port, config.User, config.Password, config.Dbname)
+	}
 
 	db, err := sql.Open("postgres", connectionString)
 
